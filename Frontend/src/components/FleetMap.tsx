41e4dc5c334a371ad8688b957e3cbb2f90e6112e
arnes <arneseufert@web.de>
2026-05-24 02:56:10 +0200

    feat: update for the route optimization
    Signed-off-by: arnes <arneseufert@web.de>

--- Frontend/src/components/FleetMap.tsx
+++ Frontend/src/components/FleetMap.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import L from "leaflet";
 
@@ -93,8 +94,8 @@ export function FleetMap({
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {trucks.map((t) => (
           <Marker
